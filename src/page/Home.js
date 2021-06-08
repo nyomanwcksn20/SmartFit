46966ff28313638af5e7c6d80f5a49d6afc44ca3
nyoman wicaksana <nyomanello42@gmail.com>
2021-06-08 19:11:47 +0700

    Update home

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,38 +1,51 @@
 import React, { Component } from 'react'
-import { Image,StyleSheet,ScrollView,Text, View, TouchableOpacity } from 'react-native'
+import { FlatList,Image,StyleSheet,ScrollView,Text, View, TouchableOpacity } from 'react-native'
+import avatar from "../asset/avatar.jpg";
 import barbel from "../asset/barbel.png";
 import yoga from "../asset/yoga.png";
-
+import fitnesstest from "../asset/fitnesstest.jpg";
 export class Home extends Component {
+
   render() {
     return (
       
-      <ScrollView style={{ flex: 1, }}>
-          <TouchableOpacity  onPress={()=>{this.props.navigation.navigate('Fitness')}}> 
-          <View style={{alignItems:'center',justifyContent:'center'}}>
-              <Image source={{uri:'https://i.pinimg.com/originals/30/7b/f5/307bf50af25dfdf4d9052476923aed44.jpg'}} style={styles.img} />
-          </View>
-          </TouchableOpacity>
-        <View style={styles.container}>
+      <View style={{ flex: 1, }}>
 
-          <TouchableOpacity style={styles.box}  onPress={()=>{this.props.navigation.navigate('Profile')}}>
-            <View style={styles.inner}>
-            <Image source={yoga} style={styles.imgbtn}/>
-              <Text style={{color:"white",margin:10}}>Senam Lantai</Text>
+        <View style={{ margin: 10,}}>
+            <View style={{flexDirection:"row"}}>
+                <Image source={avatar} style={styles.imgprofile} />
+                <View style={styles.txtprofile}>
+                    <Text style={{ fontSize: 20}}>Hallo, NamaUser</Text>
+                </View>
             </View>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.box}  onPress={()=>{this.props.navigation.navigate('Profile')}}>
-            <View style={styles.inner}>
-              <Image source={barbel} style={styles.imgbtn}/>
-              <Text style={{color:"white",margin:10}}>Angkat Barbel</Text>
-            </View>
-          </TouchableOpacity>
-
-          
-
         </View>
-    </ScrollView>
+
+          <TouchableOpacity  onPress={()=>{this.props.navigation.navigate('Fitness')}}> 
+           <View style={{alignItems:'center',justifyContent:'center'}}>
+              <Image source={fitnesstest} style={styles.img} />
+            </View>
+          </TouchableOpacity>
+
+          <ScrollView>
+            <View style={styles.container}>
+
+              <TouchableOpacity style={styles.box}  onPress={()=>{this.props.navigation.navigate('Profile')}}>
+                <View style={styles.inner}>
+                <Image source={yoga} style={styles.imgbtn}/>
+                  <Text style={{color:"white",margin:10}}>Senam Lantai</Text>
+                </View>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.box}  onPress={()=>{this.props.navigation.navigate('Profile')}}>
+                <View style={styles.inner}>
+                  <Image source={barbel} style={styles.imgbtn}/>
+                  <Text style={{color:"white",margin:10}}>Angkat Barbel</Text>
+                </View>
+              </TouchableOpacity>
+
+            </View>
+          </ScrollView>
+    </View>
     )
   }
 }
@@ -63,9 +76,24 @@ const styles = StyleSheet.create({
 
   img: {
     borderRadius: 10,
+
       marginTop: 5,
       width:"95%",
       height:150, 
+  },
+
+  imgprofile: {
+    borderRadius: 50,
+      marginTop: 5,
+      width:75,
+      height:75, 
+  },
+
+  txtprofile: {
+      marginTop: 5,
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingLeft:10, 
   },
 
   imgbtn: {
--- src/router/TabNavigator.js
+++ src/router/TabNavigator.js
@@ -1,12 +1,36 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { FitnessStackNavigator, MainStackNavigator, ProfileStackNavigator } from "./Router";
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === 'Home') {
+          iconName = focused
+            ? 'home'
+            : 'home';
+        } else if (route.name === 'Fitness') {
+          iconName = focused ? 'fitness' : 'fitness';
+        } else if (route.name === 'Profile') {
+          iconName = focused ? 'list' : 'list';
+        }
+
+        // You can return any component that you like here!
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+    })}
+    tabBarOptions={{
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    }}
+  >
       <Tab.Screen name="Home" component={MainStackNavigator} />
       <Tab.Screen name="Fitness" component={FitnessStackNavigator} />
       <Tab.Screen  name="Profile" component={ProfileStackNavigator} />

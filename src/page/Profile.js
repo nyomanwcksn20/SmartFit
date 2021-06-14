00e15dcdc6e852440dabaf76dcb39a59e98dc88f
nyoman wicaksana <nyomanello42@gmail.com>
2021-06-14 09:06:12 +0700

    add profile (not finished)

--- src/page/Profile.js
+++ src/page/Profile.js
@@ -1,19 +1,100 @@
 import React, { Component } from 'react'
-import { Button, Text, View } from 'react-native'
-import { Header } from 'react-native-elements';
+import { FlatList,Image,StyleSheet,Text, View, TouchableOpacity } from 'react-native'
+import avatar from "../asset/avatar.jpg";
 
 export class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        dataFlatList:{}
+    }
+}
+
     render() {
         return (
-        <View style={{ flex: 1, }}>
-     
-            <Button
-              onPress={()=>{this.props.navigation.navigate('History')}}
-              title="Go to History"
-            />
+            <View style={{ flex: 1, }}>
+
+            <View style={{ margin: 10,}}>
+                <View style={{flexDirection:"row"}}>
+                    <Image source={avatar} style={styles.imgprofile} />
+                    <View style={styles.txtprofile}>
+                        <Text style={{ fontSize: 20}}>NamaUser</Text>
+                    </View>
+                </View>
+            </View>
+
+            {/* <View>
+                <FlatList
+                    data={this.state.dataFlatList}
+                    keyExtractor={item=>parseInt(item.id)}
+                    renderItem={({item})=>(
+                        <View style={{borderWidth:5,borderColor:"red",flexDirection:"row",margin:5}}>
+                            <Image style={{width:100,height:100}}
+                                source={{uri:`http://192.168.100.88:8080/laporan/image/${item.image}`}}
+                            />
+                            <View style={{flexDirection:"column",alignSelf:"center"}}>
+                                <Text>Angkat Barbel : {item.status}</Text>
+                                <Text>Senam Lantai : {item.jam}</Text>
+                            </View>
+                        </View>
+                    )}
+                />
+            </View> */}
         </View>
         )
     }
 }
 
 export default Profile
+
+
+const styles = StyleSheet.create({
+    container: {
+      width:'100%',
+      height:'85%',
+      padding:5,
+      flexDirection:'row',
+      flexWrap:'wrap',
+    },
+  
+    box: {
+      width:'50%',
+      height:'50%',
+      padding:5,
+    },
+  
+    inner: {
+      flex:1,
+      backgroundColor:'dimgray',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+  
+    img: {
+      borderRadius: 10,
+  
+        marginTop: 5,
+        width:"95%",
+        height:150, 
+    },
+  
+    imgprofile: {
+      borderRadius: 50,
+        marginTop: 5,
+        width:75,
+        height:75, 
+    },
+  
+    txtprofile: {
+        marginTop: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingLeft:10, 
+    },
+  
+    imgbtn: {
+      margin: 10,
+      width:100,
+        height:100,
+    }
+  })
